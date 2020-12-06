51f1cc30cfa444e3a434b13a81ef746a4f79786e
Catalina <catalina@iMac-de-Catalina.local>
2020-12-05 23:38:19 -0600

    Inicio

---
+++ covidStatus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covidStatus
+//
+//  Created by Catalina on 05/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
